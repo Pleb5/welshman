84fe909ea939ccaa5255b9c16f79fe2cbef5ecfc
Five <co22rkn@protonmail.com>
2025-04-04 15:14:19 +0200

    Package.json modified automatically, Added nix flake files

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+description = ''A flake that creates a devShell containing the following:
+			- Nixvim (based on nixos-unstable)
+            - NodeJS
+            - PNPM
+            - Turbo
+            - Just (project maintenance/cleanup scripts)
+		'';
+
+inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixvim.url = "github:Pleb5/neovim-flake/master";
+};
+
+outputs = { self, nixpkgs, flake-utils, nixvim, ... }:
+
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+            pkgs = nixpkgs.legacyPackages.${system};
+            nvim = nixvim.packages.${system}.nvim;
+        in {
+            devShell = pkgs.mkShell {
+                buildInputs = [ 
+                    nvim
+                    pkgs.ripgrep
+                    pkgs.nodejs_20 
+                    pkgs.nodePackages.pnpm 
+                    pkgs.turbo 
+                    pkgs.just
+                    pkgs.cargo
+                    pkgs.pkg-config
+                    pkgs.openssl
+                ];
+                shellHook = ''
+                    # for ngit
+                    export PATH="$HOME/.cargo/bin:$PATH"
+                    # for linking welshman
+                    export PNPM_HOME=$HOME/.pnpm-global/bin
+                    export PATH="$HOME/.pnpm-global/bin:$PATH"
+                '';
+            };
+        }
+    );    
+}
